3ad534f82dcb789d253ad04955a3bebd44c671a9
ahmetalncak <ahmetalncak@gmail.com>
2017-12-01 12:26:51 +0300

    #51: mystrstr test files added.

---
+++ tests/MyStrStrTest.cpp
@@ -0,0 +1,22 @@
+#include "CppUTest/TestHarness.h"
+
+extern "C"
+{
+#include "mystrstr.h"
+}
+
+TEST_GROUP(MyStrStr)
+{
+	void setup()
+	{
+	}
+
+	void teardown()
+	{
+	}
+};
+
+TEST(MyStrStr, test1)
+{
+
+}
